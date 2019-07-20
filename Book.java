8e7c4077614f8ad9be932a09e399cdb87ffb1c5e
yashmaheshwari11 <yashsmaheshwari98@gmail.com>
2019-07-20 11:23:45 -0700

    Added setter and getter methods for bookId

--- Book.java
+++ Book.java
@@ -1,8 +1,17 @@
 public class Book
 {
+	private int bookId;
+	private String title;
+	private String author;
+	private float price;
+	
+	public void setBookId(int bookId){
+	this.bookId=bookId;
+	}
 
-private int bookId;
-private String title;
-private String author;
-private float price;
+	public int getBookId(){
+	return bookId;
+	}
+
+
 }
